637f253f43c03a6178d0109bc7e204cb71e98953
malleswarchinta92 <malleswar.chinta@gmail.com>
2020-12-06 09:41:09 +0530

    added jump and moving platforms

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const platformCount = 5
     let doodlerBottomSpace = 100
     let doodlerLeftSpace = 100
-    let gameOver = false
+    let isGameOver = false
+    let upTimerId, downTimerId
 
     class Platform {
         constructor(left, bottom) {
@@ -23,6 +24,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const createDoodler = () => {
         doodler.classList.add('doodler')
         grid.appendChild(doodler)
+        doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + "px"
         doodler.style.bottom = doodlerBottomSpace + "px"
     }
@@ -35,8 +37,53 @@ document.addEventListener('DOMContentLoaded', ()=> {
         }
     }
 
-    if (!gameOver) {
-        createDoodler()
+    const movePlatforms = () => {
+        platforms.forEach(platform=> {
+            platform.bottom -= 4;
+            const visual = platform.visual
+            visual.style.bottom = platform.bottom + "px"
+            if (platform.bottom < 10) {
+                platforms.shift()
+                visual.classList.remove('platform')
+                const left = Math.round(315 * Math.random(400-85))
+                const newPlatform = new Platform(left, 600)
+                platforms.push(newPlatform)
+            }
+        })
+    }
+
+    const jump = () => {
+        clearInterval(downTimerId)
+        upTimerId = setInterval(()=> {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + "px"
+            if (doodlerBottomSpace > 350) {
+                fall()
+            }
+        }, 100)
+    }
+
+    const fall = () => {
+        clearInterval(upTimerId)
+        downTimerId = setInterval(()=> {
+            doodlerBottomSpace -= 20
+            doodler.style.bottom = doodlerBottomSpace + "px"
+            if (doodlerBottomSpace <= 0) {
+                gameOver()
+            }
+        }, 100)
+    }
+
+    const gameOver = () => {
+        isGameOver = true
+        clearInterval(downTimerId)
+        clearInterval(upTimerId)
+    }
+
+    if (!isGameOver) {
         createPlatforms()
+        createDoodler()
+        setInterval(movePlatforms, 100)
+        jump()
     }
 })
